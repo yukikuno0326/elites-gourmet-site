ec6c0684a3cfb6b1c675baf101dd6b06497fe94b
KIKU <kikuno-yuki@av.avex.co.jp>
2017-01-22 13:53:20 +0000

    01_実習

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  mount_uploader :thumbnail, UserThumbnailUploader
+  
+  validates :name,      presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
+end
